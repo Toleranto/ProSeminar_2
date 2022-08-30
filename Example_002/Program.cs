198329e5dc835dccf2a3c09ec2d16f60a4d958f3
Eugenius <eugenius8447@gmail.com>
2022-08-31 00:17:31 +0300

    BoulingNumber

---
+++ Example_002/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу которая рандомно берет 
+//3-х значное число и удаляет 2-ю цифру
+//
+//int num = new Random().Next(100, 1000);
+//int hund = num / 100;
+//int ones = (num-hund*100) % 10;
+//Console.WriteLine(num);
+//Console.WriteLine($"Получившееся число->{a1}{a3}");
+
+int num = new Random().Next(100, 1000);
+int hund = num / 100;
+int ones = num % 10; 
+int result= hund * 10 + ones;
+Console.WriteLine(num);
+Console.WriteLine($"Получившееся число->{result}");
