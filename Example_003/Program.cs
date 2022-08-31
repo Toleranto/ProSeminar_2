27b3a56ef330017eaec11acca424614f7c5dc73a
Eugenius <eugenius8447@gmail.com>
2022-08-31 16:48:03 +0300

    Мой вариант

--- Example_003/Program.cs
+++ Example_003/Program.cs
@@ -1,9 +1,11 @@
 ﻿
+//if (b > a) Console.WriteLine("Второе число > первого числа");
 //Написать программу которая на вход принимает 2 числа
 // проверяет кратность одного другому и выводит остаток деления.
-Console.WriteLine("Введите 2 числа:");
+Console.Clear();
+Console.WriteLine("Введите 2 числа: ");
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
-int result = a/b;
-        
-        
+int remain = a % b;
+if (remain == 0) Console.WriteLine($"Первое число кратно второму");
+else Console.WriteLine($"Остаток от деления-> {remain}");
