cc8936620cc943118a81db46fe2a2607d39a8f32
Eugenius <eugenius8447@gmail.com>
2022-08-31 15:20:30 +0300

    Multiple

---
+++ Example_004/Program.cs
@@ -0,0 +1,10 @@
+﻿//Программа проверяет кратно ли число 7 и 23
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int multiple = a % 7;
+if (multiple == 0) Console.WriteLine($"Число кратно 7");
+int multiple2 = a % 23;
+if (multiple2 == 0) Console.WriteLine($"Число кратно 23");
+
+
