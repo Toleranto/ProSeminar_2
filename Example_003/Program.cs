cd4cabe71a4947ce37222a9fce9d6b631fabe60d
Eugenius <eugenius8447@gmail.com>
2022-08-31 15:01:40 +0300

    Example_003

---
+++ Example_003/Program.cs
@@ -0,0 +1,9 @@
+﻿
+//Написать программу которая на вход принимает 2 числа
+// проверяет кратность одного другому и выводит остаток деления.
+Console.WriteLine("Введите 2 числа:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int result = a/b;
+        
+        
